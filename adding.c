25f625119ddb9dc8511bc79b3078e65e2594d8a5
Sayan Chowdhury <chowdhurysayan46@gmail.com>
2026-01-08 18:56:40 +0530

    explaination added

--- adding.c
+++ adding.c
@@ -4,3 +4,8 @@ int main(){
     printf("new file");
     return 0;
 }
+
+//So to add new file into git there are just three lines of code you need to remember
+//git add .
+//git commit -m "Whatever message you prefer"
+//git push
